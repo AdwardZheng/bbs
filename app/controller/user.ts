f3f4209ec037d252ed9701feb0e37018b9e1c2a7
AZ <panzhengpan@163.com>
2019-01-27 02:21:14 +0800

    添加参数检查

--- app/controller/user.ts
+++ app/controller/user.ts
@@ -5,6 +5,15 @@ export default class UserController extends Controller {
         const { ctx, service } = this;
         const paramas = ctx.query;
         const { name, email, password } = paramas;
+
+        // 检查参数
+        const createRule = {
+            name: { type: 'string', required: true, trim: true },
+            email: { type: 'string', required: true, trim: true },
+            password: { type: 'string', required: true, trim: true },
+        };
+
+        ctx.validate(createRule, paramas);
         const user = await service.user.getUserByPointWord({
             $or: [
                 { name },
